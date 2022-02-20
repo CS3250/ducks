3bb3803caab629edd4c642cdeb33987fc48014e5
Matthewjww <Matthewjww@gmail.com>
2022-02-19 20:10:14 -0700

    Added OrcDuck with GruntQuack, added to Miniducksim. and fixed a duplicate variable in miniduck sim.

--- MiniDuckSimulator.java
+++ MiniDuckSimulator.java
@@ -33,10 +33,16 @@ class MiniDuckSimulator {
     nd.swim();
 
     System.out.println("\n===================");
-    Duck nd = new RageDuck();
-    nd.display();
-    nd.performQuack();
-    nd.swim();
+    Duck rad = new RageDuck();
+    rad.display();
+    rad.performQuack();
+    rad.swim();
+
+    System.out.println("\n===================");
+    Duck od = new OrcDuck();
+    od.display();
+    od.performQuack();
+    od.swim();
 
   }
 }
